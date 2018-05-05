4f59f5c4baa0e34682ed706d08a42dcdd279b409
inhochoi <inhochoi@coupang.com>
2018-05-05 19:36:37 +0900

    first commit

---
+++ src/main/java/com/deprommet/mini2/team6/Team6Application.java
@@ -0,0 +1,12 @@
+package com.deprommet.mini2.team6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team6Application.class, args);
+	}
+}
---
+++ src/test/java/com/deprommet/mini2/team6/Team6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deprommet.mini2.team6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Team6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
